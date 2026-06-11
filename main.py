30a6597cf84db56d0ee529b769f018d076807a1f
Ivan Bodnar <i539830@fontysict.nl>
2026-06-11 13:33:56 +0200

    housing requirements change

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from vestide import Vestide
 from xior import Xior
 
 AREA = "eindhoven"
-PRICE = [400, 1400]
+PRICE = [400, 1200]
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.9",
